624dae15c3484abf427e65a7fb9a183eb6c8e8c3
paunzz <bogdanbokipaunovic@gmail.com>
2024-05-04 17:12:03 +0200

    code

--- Kod/routes.py
+++ Kod/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, redirect, url_for, request, session
-from mappings.user import User, db
+from Kod.mappings.user import User
+
 
 def login():
     if request.method == 'POST':
