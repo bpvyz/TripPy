2fb3ce08bc4bcb047ce41ce8e0ec314cb9868aef
paunzz <bogdanbokipaunovic@gmail.com>
2024-05-04 18:29:29 +0200

    register route

--- Kod/app.py
+++ Kod/app.py
@@ -1,5 +1,5 @@
 from init import create_app
-from routes import login, dashboard, logout
+from routes import login, dashboard, logout, register
 
 app = create_app()
 
@@ -8,6 +8,7 @@ app.add_url_rule('/', 'login', login)
 app.add_url_rule('/login', 'login', login, methods=['GET', 'POST'])
 app.add_url_rule('/dashboard', 'dashboard', dashboard)
 app.add_url_rule('/logout', 'logout', logout)
+app.add_url_rule('/register', 'register', register, methods=['GET', 'POST'])
 
 if __name__ == '__main__':
     app.run(debug=True)
