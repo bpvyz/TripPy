f4bef0360a9a62ca9b7a699fab1143c5ab8e194a
Milan <milanstanimirovic@elfak.rs>
2024-05-06 19:26:14 +0200

    funkcionalnosti za oglase

--- Kod/routes.py
+++ Kod/routes.py
@@ -172,48 +172,99 @@ def vlasnik_dashboard():
             return render_template('vlasnik.html', user=user)
     return redirect(url_for('login'))
 
-def show_all_routes():
+def putnik_show_all_routes():
+    if 'user_id' in session:
 
-    routes = Route.query.all()
-    return render_template('show_all_routes.html', routes=routes)
+        routes = Route.query.all()
+        return render_template('putnik_show_all_routes.html', routes=routes)
+    return redirect(url_for('login'))
 
-def show_my_routes():
+def putnik_show_my_routes():
     if 'user_id' in session:
         user_id = session['user_id']
         routes = Route.query.filter_by(createdby=user_id).all()
-        return render_template('show_my_routes.html', routes=routes, user_id=user_id)
+        return render_template('putnik_show_my_routes.html', routes=routes, user_id=user_id)
+    return redirect(url_for('login'))
 
-def show_businesses():
-    try:
-        businesses = Business.query.all()
-        return render_template('show_businesses.html', businesses=businesses)
-    except Exception as e:
-        return str(e)
+def putnik_show_businesses():
+    if 'user_id' in session:
+        try:
+            businesses = Business.query.all()
+            return render_template('putnik_show_businesses.html', businesses=businesses)
+        except Exception as e:
+            return str(e)
+    return redirect(url_for('login'))
 
+"""def putnik_add_route():
+    if 'user_id' in session:
+        if request.method == 'POST':
+            routename = request.form['routename']
+            description = request.form['description']
+            startdate = request.form['startdate']
+            enddate = request.form['enddate']
 
-def add_route():
-    if request.method == 'POST':
-        routename = request.form['routename']
-        description = request.form['description']
-        startdate = request.form['startdate']
-        enddate = request.form['enddate']
+            user_id = session.get('user_id')
+            if user_id:
+                user = User.query.get(user_id)
+                if user:
 
-        user_id = session.get('user_id')
-        if user_id:
-            user = User.query.get(user_id)
-            if user:
+                    new_route = Route(
+                        routename=routename,
+                        description=description,
+                        startdate=startdate,
+                        enddate=enddate,
+                        createdby=user.userid
+                    )
+                    db.session.add(new_route)
+                    db.session.commit()
+                    return redirect(url_for('putnik_dashboard'))
 
-                new_route = Route(
-                    routename=routename,
-                    description=description,
-                    startdate=startdate,
-                    enddate=enddate,
-                    createdby=user.userid
-                )
-                db.session.add(new_route)
-                db.session.commit()
-                return redirect(url_for('putnik_dashboard'))
+        return render_template('putnik_add_route.html')
+    return redirect(url_for('login'))"""
 
-        return render_template('add_route.html', error='Failed to add route. Please try again.')
+def vlasnik_add_business():
+    if 'user_id' in session:
+        if request.method == 'POST':
+            businessname = request.form['businessname']
+            description = request.form['description']
+            locationid = request.form['locationid']
+            user_id = session.get('user_id')
+            if user_id:
+                user = User.query.get(user_id)
+                if user:
 
-    return render_template('add_route.html')
+                    new_business = Business(
+                        businessname=businessname,
+                        description=description,
+                        locationid=int(locationid),
+                        ownerid=user.userid
+                    )
+                    db.session.add(new_business)
+                    db.session.commit()
+                    return redirect(url_for('vlasnik_dashboard'))
+
+        return render_template('vlasnik_add_business.html')
+    return redirect(url_for('login'))
+
+def vlasnik_show_all_businesses():
+    if 'user_id' in session:
+        try:
+            businesses = Business.query.all()
+            return render_template('vlasnik_show_all_businesses.html', businesses=businesses)
+        except Exception as e:
+            return str(e)
+    return redirect(url_for('login'))
+
+def vlasnik_show_my_businesses():
+    if 'user_id' in session:
+        user_id = session['user_id']
+        businesses = Business.query.filter_by(ownerid=user_id).all()
+        return render_template('vlasnik_show_my_businesses.html', businesses=businesses, user_id=user_id)
+    return redirect(url_for('login'))
+
+def vlasnik_show_routes():
+    if 'user_id' in session:
+
+        routes = Route.query.all()
+        return render_template('vlasnik_show_routes.html', routes=routes)
+    return redirect(url_for('login'))
